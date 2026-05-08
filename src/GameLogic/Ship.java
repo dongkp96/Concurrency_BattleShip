de65e6bb966258c7d608a70da132ae89c64ff4aa
Kevin Dong <kdmac@macbookpro.lan>
2026-05-08 18:52:35 -0400

    Adds Ship class and its methods

--- src/GameLogic/Ship.java
+++ src/GameLogic/Ship.java
@@ -6,32 +6,48 @@ public class Ship{
 
     private ShipType type;
     private int hitCount;
-    private boolean isSunk = false;
     private ArrayList<int []> coordinates;
 
-    public Ship(ShipType type){
+    public Ship(ShipType type, ArrayList<int []> shipCoordinates){
         this.type = type;
         this.hitCount = 0;
-        this.coordinates = new ArrayList<>();
+        this.coordinates = shipCoordinates;
     }
 
     /**
      * @return a boolean to check if a ship has been sunk
      * */
     public boolean checkIsSunk(){
-        return this.isSunk;
+        if(this.hitCount == this.type.getSize()){
+            return true;
+        }
+        return false;
     }
 
     /**
      * Used to increase hit counter, will
+     * change status of isSunk if hit counter exceeds ship size
      * */
     public void increaseHitCount(){
         this.hitCount ++;
-        if(this.hitCount == this.type.getSize()){
-            this.isSunk = true;
-        }
     }
 
+
+    /**
+     * @param coordinate, an integer array containing a pair of integers
+     *                    for coordinates to be checked if its inside the
+     *                    ships coordinates
+     * @return boolean to confirm or deny a hit
+     * */
+    public boolean checkIfHit(int[] coordinate){
+        for(int i = 0; i<this.coordinates.size(); i++){
+            if((this.coordinates.get(i)[0]==coordinate[0])&&(this.coordinates.get(i)[1]== coordinate[1])){
+                this.increaseHitCount();
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
