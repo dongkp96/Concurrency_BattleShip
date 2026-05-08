2ee90c9eb821244fdea4973a545cb173e0c8ed46
Kevin Dong <kdmac@macbookpro.lan>
2026-05-08 08:46:35 -0400

    Adds ShipType enum code

---
+++ src/GameLogic/ShipType.java
@@ -0,0 +1,30 @@
+public enum ShipType{
+
+    CARRIER(5),
+    BATTLESHIP(4),
+    CRUISER(3),
+    SUBMARINE(3),
+    DESTROYER(2);
+    /*
+    * These are enum constants that once called
+    * by the enum ship type such as ShipType.CARRIER,
+    * it will call the constructor and set the field
+    * */
+
+    public final int size;
+    //field to hold the size, final because it won't change
+
+    private ShipType(int size){
+        this.size = size;
+    }
+    //constructor to set size for the enum, private because it needs to be for enums
+
+    /**
+     * @Return an integer that indicates the Ship type's size
+     * */
+    public int getSize(){
+        return this.size;
+    }
+
+
+}
